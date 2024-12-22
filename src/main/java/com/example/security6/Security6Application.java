99fd0a84d69a494a12830f36ef96ce41e6f1c4b4
JinHyun Park <wlsgus1149@naver.com>
2024-12-22 21:55:05 +0900

    first

---
+++ src/main/java/com/example/security6/Security6Application.java
@@ -0,0 +1,13 @@
+package com.example.security6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Security6Application.class, args);
+    }
+
+}
