e98aa4a5f732f3d5963a87c5f2d13c161de78a98
JinHyun Park <wlsgus1149@naver.com>
2024-12-23 22:59:54 +0900

    SecurityConfig 추가

---
+++ src/main/java/com/example/security6/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.security6.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
